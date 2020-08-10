de55aa0148b4dac19cd7124d7d3b7cb2fba0c3f2
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-08-10 16:53:19 -0300

    camera following the player only when it defies the frustrum

--- Assets/Scripts/Camera/CameraFollower.cs
+++ Assets/Scripts/Camera/CameraFollower.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class CameraFollower : SingletonBase<CameraFollower>
 {
     Vector3 moveVector;
+    Vector3 playerOnScreen;
     float camVertExtent;
     float camHorzExtent;
     float leftConstraint;
     float rightConstraint;
     float topConstraint;
     float bottomConstraint;
+    public float playerCameraLimitRight;
+    public float playerCameraLimitLeft;
+    public float playerCameraLimitUp;
+    public float playerCameraLimitDown;
+    public float cameraTranslationSpeed;
 
     void CalculateCameraLimits()
     {
@@ -26,6 +32,7 @@ public class CameraFollower : SingletonBase<CameraFollower>
     {
         base.SingletonAwake();
         moveVector = Vector3.zero;
+        playerOnScreen = Vector3.zero;
         CalculateCameraLimits();
     }
 
@@ -34,10 +41,49 @@ public class CameraFollower : SingletonBase<CameraFollower>
         SingletonAwake();
     }
 
+    bool CheckIfPlayerIsInCamera() 
+    {
+        playerOnScreen = this.GetComponent<Camera>().WorldToViewportPoint(PlayerPosition.instance.transform.position);
+        if (playerOnScreen.x > playerCameraLimitLeft && playerOnScreen.x < playerCameraLimitRight && playerOnScreen.y > playerCameraLimitDown && playerOnScreen.y < playerCameraLimitUp) 
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void FollowX() 
+    {
+        if (playerOnScreen.x < playerCameraLimitLeft) 
+        {
+            moveVector.x -= cameraTranslationSpeed * Time.deltaTime;
+        }
+        else if (playerOnScreen.x > playerCameraLimitRight) 
+        {
+            moveVector.x += cameraTranslationSpeed * Time.deltaTime;
+        }
+        moveVector.x = Mathf.Clamp(moveVector.x, leftConstraint, rightConstraint);
+    }
+
+    void FollowY() 
+    {
+        if (playerOnScreen.y < playerCameraLimitDown)
+        {
+            moveVector.y -= cameraTranslationSpeed * Time.deltaTime;
+        }
+        else if (playerOnScreen.y > playerCameraLimitUp)
+        {
+            moveVector.y += cameraTranslationSpeed * Time.deltaTime;
+        }
+        moveVector.y = Mathf.Clamp(moveVector.y, leftConstraint, rightConstraint);
+    }
+
     void Follow()
     {
-        moveVector.x = Mathf.Clamp(PlayerPosition.instance.transform.position.x, leftConstraint, rightConstraint);
-        moveVector.y = Mathf.Clamp(PlayerPosition.instance.transform.position.y, bottomConstraint, topConstraint);
+        if (!CheckIfPlayerIsInCamera()) 
+        {
+            FollowX();
+            FollowY();
+        }
         moveVector.z = this.gameObject.transform.position.z;
         transform.position = moveVector;
     }
