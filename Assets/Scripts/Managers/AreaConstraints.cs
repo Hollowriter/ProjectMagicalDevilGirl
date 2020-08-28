9e2aa8b3eafbb01b82bce828e3bbb5bfbef65fe3
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-08-28 15:29:27 -0300

    starting a real time constrainer

--- Assets/Scripts/Managers/AreaConstraints.cs
+++ Assets/Scripts/Managers/AreaConstraints.cs
@@ -13,4 +13,24 @@ public class AreaConstraints : SingletonBase<AreaConstraints>
     {
         SingletonAwake();
     }
+
+    public void SetLeftStageLimit(float _LeftStageLimit) 
+    {
+        LeftStageLimit = _LeftStageLimit;
+    }
+
+    public void SetRightStageLimit(float _RightStageLimit) 
+    {
+        RightStageLimit = _RightStageLimit;
+    }
+
+    public void SetTopStageLimit(float _TopStageLimit) 
+    {
+        TopStageLimit = _TopStageLimit;
+    }
+
+    public void SetBottomStageLimit(float _BottomStageLimit) 
+    {
+        BottomStageLimit = _BottomStageLimit;
+    }
 }
