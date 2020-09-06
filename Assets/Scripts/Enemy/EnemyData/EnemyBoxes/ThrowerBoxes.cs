09f616ca4051993f92cdf2a3eb73b7cb312e69c4
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-09-06 16:10:56 -0300

    Enemy thrower done

---
+++ Assets/Scripts/Enemy/EnemyData/EnemyBoxes/ThrowerBoxes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowerBoxes : EnemyBoxes
+{
+    private void Awake()
+    {
+        Begin();
+    }
+
+    protected override void AttackBox()
+    {
+        if (!attackBox.activeInHierarchy && enemyStates.GetState() == (int)EnemyStates.BaddieStates.AttackingPlayer) 
+        {
+            attackBox.SetActive(true);
+            attackBoxTimer = 0;
+        }
+        if (attackBox.activeInHierarchy) 
+        {
+            attackBoxTimer += attackBoxSpeed * Time.deltaTime;
+            if (attackBoxTimer >= attackBoxTime) 
+            {
+                attackBoxTimer = 0;
+                attackBox.SetActive(false);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        Behave();
+    }
+}
